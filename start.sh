e09f23974da183089357d15a5d83c4287a076caa
Danilo <danilo.cavallini.01@gmail.com>
2024-05-13 12:01:31 +0200

    starting bash edits

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-/usr/bin/g++ -Ofast -std=c++17 -fdiagnostics-color=always -I . -I $HOME/boost_1_84_0/  -g ./*.cpp -g ./*.hpp -o main && ./main
+/usr/bin/g++ -Ofast -std=c++17 -fdiagnostics-color=always -I . -I $HOME/boost_1_84_0/ -pthread  -g ./*.cpp -g ./*.hpp -o main
